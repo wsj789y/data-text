1e81f0e6241011f94c887419af2c276defb37bda
wushuaijie <wushuaijie@fengchaotech.cn>
2018-08-09 18:07:44 +0800

    linkedMap adn BSTMap

---
+++ src/com/leetCode/Solution803.java
@@ -0,0 +1,20 @@
+package com.leetCode;
+
+import java.util.TreeSet;
+
+public class Solution803 {
+    public int uniqueMorseRepresentations(String[] words) {
+
+        String[] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        TreeSet<String> set = new TreeSet<>();
+        for(String word: words){
+            StringBuilder res = new StringBuilder();
+            for(int i = 0 ; i < word.length() ; i ++)
+                res.append(codes[word.charAt(i) - 'a']);
+
+            set.add(res.toString());
+        }
+
+        return set.size();
+    }
+}
---
+++ src/com/set/Set.java
@@ -0,0 +1,11 @@
+package com.set;
+
+public interface Set<E> {
+
+    void add(E e);
+    boolean contains(E e);
+    void remove(E e);
+    int getSize();
+    boolean isEmpty();
+}
+
